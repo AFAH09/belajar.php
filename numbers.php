b9a8cb059524cd4662407f5bcd76202fbb9cb931
jajijujang <ahmadfauzanarifhakim@gmail.com>
2023-05-12 07:27:16 +0700

    upload terbaru php

---
+++ numbers.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP Project</title>
+</head>
+
+<body>
+    <?php
+//     //penjumlahan
+//     //cara 1
+//    echo 5 + 5;
+//     //cera 2
+//    $a =10;
+//    $b =5;
+//    echo $a + $b
+
+   // pangkat
+   $angka =5;
+   $pangkat =4;
+   echo pow ($angka, $pangkat);
+    ?>
+</body>
+
+</html>
